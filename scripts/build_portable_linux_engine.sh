d9dde695e8b95c747078a658c06c7dd8d9c27559
cstom4994 <2480707737@qq.com>
2019-07-11 20:03:13 +0800

    modified:   CMakeLists.txt 	modified:   scripts/build_portable_linux_engine.sh 	modified:   scripts/travis_linux_deps.sh

--- scripts/build_portable_linux_engine.sh
+++ scripts/build_portable_linux_engine.sh
@@ -1,41 +1,3 @@
-#!/bin/bash
-
-# Build custom SDL2
-
-cd $TRAVIS_BUILD_DIR/SDL2-2.0.7
-export CC="ccache gcc -msse2 -march=i686 -m32 -ggdb -O2"
-./configure \
-	--disable-dependency-tracking \
-	--enable-audio \
-	--enable-video \
-	--enable-events \
-	--disable-render \
-	--enable-joystick \
-	--disable-haptic \
-	--disable-power \
-	--enable-threads \
-	--enable-timers \
-	--enable-loadso \
-	--enable-video-opengl \
-	--enable-x11-shared \
-	--enable-video-x11 \
-	--enable-video-x11-xrandr \
-	--enable-video-x11-scrnsaver \
-	--enable-video-x11-xinput \
-	--disable-video-x11-xshape \
-	--disable-video-x11-xdbe \
-	--disable-libudev \
-	--disable-dbus \
-	--disable-ibus \
-	--enable-sdl-dlopen \
-	--disable-video-opengles \
-	--disable-cpuinfo \
-	--disable-assembly \
-	--disable-atomic \
-	--enable-alsa
-make -j2
-mkdir -p $TRAVIS_BUILD_DIR/sdl2-linux
-make install DESTDIR=$TRAVIS_BUILD_DIR/sdl2-linux
 
 # Build engine
 
@@ -46,7 +8,7 @@ export CXX="ccache g++"
 export CFLAGS="-m32"
 export CXXFLAGS="-m32"
 cmake \
-	-DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sdl2-linux/usr/local \
+	-DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sdl2/usr/local \
 	-DXASH_DOWNLOAD_DEPENDENCIES=yes \
 	-DXASH_STATIC=ON \
 	-DXASH_DLL_LOADER=ON \
@@ -55,5 +17,5 @@ cmake \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo ../
 make -j2 VERBOSE=1
 cp engine/csmoe mainui/libxashmenu.so vgui_support/libvgui_support.so vgui_support/vgui.so .
-cp $TRAVIS_BUILD_DIR/sdl2-linux/usr/local/lib/$(readlink $TRAVIS_BUILD_DIR/sdl2-linux/usr/local/lib/libSDL2-2.0.so.0) libSDL2-2.0.so.0
+cp $TRAVIS_BUILD_DIR/sdl2/usr/local/lib/$(readlink $TRAVIS_BUILD_DIR/sdl2/usr/local/lib/libSDL2-2.0.so.0) libSDL2-2.0.so.0
 7z a -t7z $TRAVIS_BUILD_DIR/csmoe-linux.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on csmoe libSDL2-2.0.so.0 libvgui_support.so vgui.so libxashmenu.so
--- scripts/travis_linux_deps.sh
+++ scripts/travis_linux_deps.sh
@@ -1,9 +1,2 @@
-#!/bin/sh
-
-
-# Disabled. Use deps autodownload
-# curl -s http://libsdl.org/release/SDL2-devel-2.0.7-mingw.tar.gz | tar xzf -
-# mv SDL2-2.0.7 sdl2-mingw
-
-curl -s http://libsdl.org/release/SDL2-2.0.7.tar.gz | tar xzf -
-
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
