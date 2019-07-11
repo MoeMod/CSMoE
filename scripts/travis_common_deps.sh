7e38ababf180d303e9626b4d40f7be1b290ee937
cstom4994 <2480707737@qq.com>
2019-07-11 19:39:53 +0800

    modified:   .travis.yml 	modified:   scripts/build_mingw_engine.sh 	new file:   scripts/build_portable_linux_engine.sh 	new file:   scripts/travis_common_deps.sh 	modified:   scripts/travis_linux_deps.sh

---
+++ scripts/travis_common_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Disabled. Use autodownload
+# git clone --depth 1 https://github.com/FWGS/vgui-dev
+# git clone --depth 1 https://github.com/FWGS/vgui_support_bin 
--- scripts/travis_linux_deps.sh
+++ scripts/travis_linux_deps.sh
@@ -1,1 +1,9 @@
-sudo apt-get install libsdl2-dev
+#!/bin/sh
+
+
+# Disabled. Use deps autodownload
+# curl -s http://libsdl.org/release/SDL2-devel-2.0.7-mingw.tar.gz | tar xzf -
+# mv SDL2-2.0.7 sdl2-mingw
+
+curl -s http://libsdl.org/release/SDL2-2.0.7.tar.gz | tar xzf -
+
