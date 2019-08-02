28d53ff65e5ea35c6ed55ebd8062c1fcef0156f1
moemod <824395314@qq.com>
2019-08-03 01:31:51 +0800

    travis ci test 3

--- scripts/build_portable_linux_engine.sh
+++ scripts/build_portable_linux_engine.sh
@@ -4,13 +4,6 @@ cd $TRAVIS_BUILD_DIR
 mkdir -p build && cd build/
 export CC="ccache gcc"
 export CXX="ccache g++"
-export CFLAGS="-m32"
-export CXXFLAGS="-m32"
 cmake \
-    -DXASH_DOWNLOAD_DEPENDENCIES=yes \
-    -DXASH_STATIC=ON \
-    -DXASH_DLL_LOADER=ON \
-    -DXASH_VGUI=ON \
-    -DMAINUI_USE_STB=ON \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
 make -j4 VERBOSE=1
