c15d4e281d4e40bffc02c882f68fcbf0f9bde143
moemod <824395314@qq.com>
2019-09-04 20:04:46 +0800

    CI : windows build test 4

--- scripts/build_windows_engine.sh
+++ scripts/build_windows_engine.sh
@@ -3,6 +3,6 @@
 cd $TRAVIS_BUILD_DIR
 mkdir -p win-build && cd win-build
 cmake \
-    -G "NMake Makefiles" \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
-nmake /nologo
+    -G "Visual Studio 15 2017 Win64" \
+    -DCMAKE_BUILD_TYPE=Release ../
+cmake --build . --config Release
