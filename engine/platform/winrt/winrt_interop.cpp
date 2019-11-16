0c37dca9fc282b1139a3eed751b8257212784ea5
MoeMod <824395314@qq.com>
2019-11-16 23:15:07 +0800

    m2 anim fix

--- engine/platform/winrt/winrt_interop.cpp
+++ engine/platform/winrt/winrt_interop.cpp
@@ -1,6 +1,7 @@
 
 #include <wrl.h>
 #include <windows.ui.ViewManagement.h>
+#include <windows.applicationmodel.core.h>
 #include <windows.System.h>
 
 #include "winrt_interop.h"
@@ -13,8 +14,19 @@ extern "C" {
 using namespace Microsoft::WRL;
 using namespace Microsoft::WRL::Wrappers;
 using namespace ABI::Windows::Foundation;
+using namespace ABI::Windows::UI;
 using namespace ABI::Windows::UI::Core;
 using namespace ABI::Windows::UI::ViewManagement;
+using namespace ABI::Windows::ApplicationModel::Core;
+
+class ColorReference : public RuntimeClass<RuntimeClassFlags<WinRt>, __FIReference_1_Windows__CUI__CColor>
+{
+public:
+	HRESULT get_Value(Color* value) override { return *value = color, S_OK; }
+	Color color;
+private:
+	~ColorReference() {}
+};
 
 void WinRT_FullscreenMode_Install(boolean fullscreen)
 {
@@ -51,6 +63,55 @@ void WinRT_FullscreenMode_Install(boolean fullscreen)
 			
 			// disable the system gestures...
 			hr = appView3->put_FullScreenSystemOverlayMode(FullScreenSystemOverlayMode_Minimal);
+
+			// change title bar color
+#if 0
+			ComPtr<IApplicationViewTitleBar> titleBar;
+			if(hr = appView3->get_TitleBar(&titleBar), SUCCEEDED(hr))
+			{
+				ComPtr<IColorHelperStatics> colorHelperStatics;
+				hr = Windows::Foundation::GetActivationFactory(
+					HStringReference(RuntimeClass_Windows_UI_Core_CoreWindow).Get(),
+					&colorHelperStatics);
+
+				static ComPtr<ColorReference> red; hr = MakeAndInitialize<ColorReference>(&red); red->color = { 255, 232, 16, 35 };
+				static ComPtr<ColorReference> red2; hr = MakeAndInitialize<ColorReference>(&red2); red2->color = { 255, 181, 113, 113 };
+				static ComPtr<ColorReference> red3; hr = MakeAndInitialize<ColorReference>(&red3); red3->color = { 255, 188, 47, 46 };
+				static ComPtr<ColorReference> red4; hr = MakeAndInitialize<ColorReference>(&red4); red4->color = { 255, 241, 112, 122 };
+				static ComPtr<ColorReference> white; hr = MakeAndInitialize<ColorReference>(&white); white->color = { 255, 255, 255, 255 };
+				static ComPtr<ColorReference> black; hr = MakeAndInitialize<ColorReference>(&black); black->color = { 255, 0, 0, 0 };
+				
+				hr = titleBar->put_BackgroundColor(red3.Get());
+				hr = titleBar->put_ButtonBackgroundColor(red3.Get());
+				hr = titleBar->put_ButtonInactiveBackgroundColor(red2.Get());
+				hr = titleBar->put_InactiveBackgroundColor(red2.Get());
+				hr = titleBar->put_ButtonHoverBackgroundColor(red.Get());
+				hr = titleBar->put_ButtonPressedBackgroundColor(red4.Get());
+				
+				hr = titleBar->put_ForegroundColor(white.Get());
+				hr = titleBar->put_ButtonForegroundColor(white.Get());
+				hr = titleBar->put_ButtonHoverForegroundColor(white.Get());
+				hr = titleBar->put_ButtonPressedForegroundColor(black.Get());
+				hr = titleBar->put_ButtonInactiveForegroundColor(white.Get());
+				hr = titleBar->put_InactiveForegroundColor(white.Get());
+			}
+#endif
+			// extend window
+			ComPtr<ICoreApplication> coreApplication;
+			hr = Windows::Foundation::GetActivationFactory(
+				HStringReference(RuntimeClass_Windows_ApplicationModel_Core_CoreApplication).Get(),
+				&coreApplication);
+
+			ComPtr<ICoreApplicationView> coreApplicationView;
+			hr = coreApplication->GetCurrentView(&coreApplicationView);
+
+			ComPtr<ICoreApplicationView3> coreApplicationView3;
+			hr = coreApplicationView.As(&coreApplicationView3);
+
+			ComPtr<ICoreApplicationViewTitleBar> coreApplicationViewTitleBar;
+			hr = coreApplicationView3->get_TitleBar(&coreApplicationViewTitleBar);
+
+			hr = coreApplicationViewTitleBar->put_ExtendViewIntoTitleBar(false);
 		}
 	}
 }
