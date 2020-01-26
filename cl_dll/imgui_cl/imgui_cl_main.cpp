6eea26a9263440e18d071afa1a3102015e025f22
moemod <824395314@qq.com>
2020-01-26 14:11:27 +0800

    ImGUI : fix CI build x5

--- cl_dll/imgui_cl/imgui_cl_main.cpp
+++ cl_dll/imgui_cl/imgui_cl_main.cpp
@@ -27,36 +27,37 @@ void DLLEXPORT HUD_OnGUI( struct ImGuiContext *context )
 
     static CImGuiBuyMenu buy;
     ImGui::SetCurrentContext(context);
-    static bool show_demo_window = false;
-    /*
-    static float f = 0.0f;
-    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	/*
+	static bool show_demo_window = false;
+	static float f = 0.0f;
+	static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    static bool show_main_window = false;
-    static bool show_debug_window = false;
+	static bool show_main_window = false;
+	static bool show_debug_window = false;
 
-    ImGui::Begin("测试窗口");
-    ImGui::Text("测试下中文显示");
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-    ImGui::ColorEdit3("clear color", (float*)&clear_color);
-    if (ImGui::Button("Demo 窗口"))
-        show_demo_window ^= 1;
-    if (ImGui::Button("测试窗口"))
-        show_debug_window ^= 1;
-    ImGui::Text("平均 %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    ImGui::End();
+	ImGui::Begin("测试窗口");
+	ImGui::Text("测试下中文显示");
+	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+	ImGui::ColorEdit3("clear color", (float*)&clear_color);
+	if (ImGui::Button("Demo 窗口"))
+		show_demo_window ^= 1;
+	if (ImGui::Button("测试窗口"))
+		show_debug_window ^= 1;
+	ImGui::Text("平均 %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::End();
 
-    if (show_debug_window)
-    {
-        ImGui::Text("Hello World!");
+	if (show_debug_window)
+	{
+		ImGui::Text("Hello World!");
 
-    }
-     */
-    if (show_demo_window)
-    {
-        ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_FirstUseEver);
-        ImGui::ShowDemoWindow(&show_demo_window);
-    }
+	}
+
+	if (show_demo_window)
+	{
+		ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_FirstUseEver);
+		ImGui::ShowDemoWindow(&show_demo_window);
+	}
+	 */
 
     buy.OnGUI();
 }
