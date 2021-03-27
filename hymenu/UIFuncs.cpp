4209da9ef188a4054768df6aeaff8c9eab27bde5
moemod <824395314@qq.com>
2021-03-27 22:15:28 +0800

    add hymenu again

---
+++ hymenu/UIFuncs.cpp
@@ -0,0 +1,75 @@
+#include "extdll_menu.h"
+#include "Utils.h"
+
+int UI_VidInit(void)
+{
+	return 0;
+}
+void UI_Init(void)
+{
+
+}
+void UI_Shutdown(void)
+{
+
+}
+void UI_UpdateMenu(float flTime)
+{
+
+}
+void UI_KeyEvent(int key, int down)
+{
+
+}
+void UI_MouseMove(int x, int y)
+{
+
+}
+void UI_SetActiveMenu(int fActive)
+{
+
+}
+void UI_AddServerToList(netadr_t adr, const char* info)
+{
+
+}
+void UI_GetCursorPos(int* pos_x, int* pos_y)
+{
+
+}
+void UI_SetCursorPos(int pos_x, int pos_y)
+{
+
+}
+void UI_ShowCursor(int show)
+{
+
+}
+void UI_CharEvent(int key)
+{
+
+}
+int UI_MouseInRect(void)
+{
+	return 0;
+}
+int UI_IsVisible(void)
+{
+	return 0;
+}
+int UI_CreditsActive(void)
+{
+	return 0;
+}
+void UI_FinalCredits(void)
+{
+
+}
+void UI_OnGUI(struct ImGuiContext* context)
+{
+
+}
+extern "C" EXPORT void AddTouchButtonToList(const char* name, const char* texture, const char* command, unsigned char* color, int flags)
+{
+
+}
