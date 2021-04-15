89aa38bc5078a8b2ed060df46a3f65855e8fcc70
hymei <hymei@tencent.com>
2021-04-15 20:00:43 +0800

    CI : update script

--- scripts/build_windows_engine.sh
+++
@@ -1,8 +0,0 @@
-# Build engine
-
-cd $TRAVIS_BUILD_DIR
-mkdir -p win-build && cd win-build
-cmake \
-    -G "Visual Studio 15 2017 Win64" \
-    -DCMAKE_BUILD_TYPE=Release ../
-cmake --build . --config Release
