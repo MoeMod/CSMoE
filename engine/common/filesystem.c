fc27afb4023f15ebee1c6087a0f1ff00d51fe485
hymei <hymei@tencent.com>
2021-04-18 02:09:51 +0800

    fix sprview

--- engine/common/filesystem.c
+++ engine/common/filesystem.c
@@ -2341,7 +2341,7 @@ searchpath_t *FS_FindFile( const char *name, int* index, qboolean gamedironly )
 		}
 	}
 
-	if( fs_ext_path && ( pEnvPath = getenv( "Path" )))
+	if( fs_ext_path )
 	{
 		char	netpath[MAX_SYSPATH];
 
@@ -2351,7 +2351,8 @@ searchpath_t *FS_FindFile( const char *name, int* index, qboolean gamedironly )
 
 		// root folder has a more priority than netpath
 		Q_strncpy( search->filename, host.rootdir, sizeof( search->filename ));
-		Q_strcat( search->filename, "\\" );
+
+		Q_strcat( search->filename, "/" );
 		Q_snprintf( netpath, MAX_SYSPATH, "%s%s", search->filename, name );
 
 		if( FS_SysFileExists( netpath, !(search->flags & FS_CUSTOM_PATH ) ) )
@@ -2362,12 +2363,13 @@ searchpath_t *FS_FindFile( const char *name, int* index, qboolean gamedironly )
 		}
 
 		// search for environment path
+        pEnvPath = getenv( "Path" );
 		while( pEnvPath )
 		{
 			char *end = Q_strchr( pEnvPath, ';' );
 			if( !end ) break;
 			Q_strncpy( search->filename, pEnvPath, (end - pEnvPath) + 1 );
-			Q_strcat( search->filename, "\\" );
+			Q_strcat( search->filename, "/" );
 			Q_snprintf( netpath, MAX_SYSPATH, "%s%s", search->filename, name );
 
 			if( FS_SysFileExists( netpath, !(search->flags & FS_CUSTOM_PATH ) ) )
